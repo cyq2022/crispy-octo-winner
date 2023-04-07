24693786121b38947e120ce99cb4cca2ecf89c66
Shoujian YUN <jiangya68@gmail.com>
2023-04-07 16:08:55 +0800

    clean

--- winner/src/Main.java
+++ winner/src/Main.java
@@ -6,12 +6,5 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
-        System.out.println("GitHub提交");
-        System.out.println("GitHub提交1");
-        System.out.println("GitHub提交2");
-        System.out.println("GitHub提交3");
-        System.out.println("GitHub提交5");
-        System.out.println("GitHub提交5");
-        System.out.println("GitHub提交6");
     }
 }
